59c4c4ee0760abb3a0404e334e7dd5db5371c3d8
Tyler887 <tylermageeshields@gmail.com>
2022-05-05 10:54:10 +0100

    Update eatinstaller.sh

--- eatinstaller.sh
+++ eatinstaller.sh
@@ -20,6 +20,38 @@ if [ -d "/home/$(whoami)/Eat-PKG-Manager" ]; then
    exit 1
 fi
 
+# Get Linux distribution and set the name of it
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    OS=SUSE
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    OS=RHEL
+else
+    dialog --title "Unsupported Linux Distribution" --msgbox "EatInstaller has detected an unsupported Linux distro. Supported distros are Debian GNU/Linux, Ubuntu, Kali, Linux Mint, openSUSE, SUSE, Pengwin, Raspberry Pi OS, Fedora, Red Hat, CentOS, other RHEL/SUSE/Debian based distributions." 1000 1000
+    clear
+    exit 1
+fi
+
+# Check distro and pick the right command for installing dependencies.
+if ! [ OS -ne "Debian" ]; then # on Debian, use apt-get
+    INSTALL_COMMAND="sudo apt-get update && sudo apt-get install git python3 python3-pip -y"
+elif ! [ OS -ne "RHEL" ]; then # om RHEL, use yum
+    INSTALL_COMMAND="sudo yum install -y python310 python310-pip git"
+elif ! [ OS -ne "SUSE" ]; then # on (open)SUSE, use zypper
+    INSTALL_COMMAND="sudo zypper refresh && sudo zypper install -y python310 python310-pip git"
+fi
+
 # Print an error if user is not in sudoers or hackers are installing eat (implemented in next line)
 autherror() {
   echo "failed to authenticate, are you in the sudoers file? The message should be above"
@@ -33,7 +65,7 @@ sudo echo -n "" || autherror
 clear
 
 # Install dependencies, return output in the UI
-dialog --title "Installing OS Dependencies" --prgbox "Eat is installing it's dependencies. This will take a while, and the progress can be seen below." "sudo apt-get update && sudo apt-get install git python3 python3-pip -y" 1000 1000
+dialog --title "Installing OS Dependencies" --prgbox "Eat is installing it's dependencies. This will take a while, and the progress can be seen below." "$INSTALL_COMMAND" 1000 1000
 dialog --title "Installing Python Dependencies" --prgbox "Eat is installing additional dependencies for Python 3." "pip install colorama distro requests pyyaml distro" 1000 1000
 clear
 
